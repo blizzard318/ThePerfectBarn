4fd5b5e1737a52f2efb22c91c36b9243c21fb49f
Andrew Chen <anew318@hotmail.com>
2024-03-20 17:48:28 +0800

    yessss

--- Assets/Scripts/GenerateMenu.cs
+++ Assets/Scripts/GenerateMenu.cs
@@ -8,18 +8,91 @@ using System.Threading.Tasks;
 
 using Google.Apis.Services;
 using Google.Apis.Sheets.v4;
-using Google.Apis.Sheets.v4.Data;
 using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4.Data;
 
 public class GenerateMenu : MonoBehaviour
 {
-    [SerializeField] private GameObject MenuItemPrefab;
+    [SerializeField] private GameObject CategoryPrefab, ItemEntryPrefab;
     [SerializeField] private RectTransform Scroll;
+    [SerializeField] private TextMeshProUGUI Subtitle;
 
     private SheetsService _sheetsService;
     private const string _spreadsheetId = "1BO_oYW57E_xQZqt2UcAIrpRa6s5ZgK_nh4UD8Q5coZQ";
+    private bool EVENT = false, Refreshed = false;
+    const string range = "Menu!A1:F";
 
-    private void ConnectToGoogle()
+    public async Task<string> UpdateData(List<List<string>> data)
+    {
+
+        var objectList = new List<object>() { "updated", "Test" };
+        var objectList2 = new List<object>() { "updated2", "Test2" };
+        var valueRange = new ValueRange();
+        valueRange.Range = range;
+        valueRange.Values = new List<IList<object>>
+        {
+            objectList,
+            objectList2
+        };
+
+        // The new values to apply to the spreadsheet.
+        var requestBody = new BatchUpdateValuesRequest()
+        {
+            ValueInputOption = "USER_ENTERED",
+            Data = new List<ValueRange> { valueRange }
+        };
+
+        var request = _sheetsService.Spreadsheets.Values.BatchUpdate(requestBody, _spreadsheetId);
+
+        var response = await request.ExecuteAsync(); // For async 
+
+        return JsonUtility.ToJson(response);
+    }
+
+    public async void OnValueChanged (Vector2 _)
+    {
+        if (!Refreshed && Scroll.anchoredPosition.y < -300) await RefreshMenu();
+        else if (Scroll.anchoredPosition.y > 0) Refreshed = false;
+    }
+
+    private async Task RefreshMenu()
+    {
+        Refreshed = true;
+        var getRequest = _sheetsService.Spreadsheets.Values.Get(_spreadsheetId, range);
+
+        var getResponse = await getRequest.ExecuteAsync();
+        var values = getResponse.Values;
+        if (values != null && values.Count > 0)
+        {
+            EVENT = string.IsNullOrWhiteSpace(values[0][0].ToString());
+            Subtitle.text = values[0][1].ToString();
+
+            for (int i = 1; i < values.Count; i++) //Skip first row, that's handled above.
+            {
+                var row = values[i];
+                if (row.Count == 0) continue;
+
+                var value = row[0].ToString();
+
+                const string CategoryPrefix = "Category:";
+                if (value.Contains(CategoryPrefix))
+                {
+                    var category = Instantiate(CategoryPrefab, Scroll);
+                    category.GetComponentInChildren<TextMeshProUGUI>().text = value.Split(CategoryPrefix)[1];
+                }
+                else
+                {
+                    var item = Instantiate(ItemEntryPrefab, Scroll);
+                    var texts = item.GetComponentsInChildren<TextMeshProUGUI>();
+                    texts[0].text = value;
+                    if (!string.IsNullOrWhiteSpace(row[1].ToString())) texts[1].text = row[1].ToString();
+                    if (!EVENT) texts[2].text = $"${row[2]} Donation";
+                }
+            }
+        }
+    }
+
+    private async void Awake()
     {
         GoogleCredential credential;
 
@@ -36,88 +109,8 @@ public class GenerateMenu : MonoBehaviour
             HttpClientInitializer = credential,
             ApplicationName = "perfect-barn"
         });
-    }
-    public async Task<string> UpdateData(List<List<string>> data)
-    {
-        var objParams = new List<IList<object>>();
-        foreach (var row in data) objParams.Add(new List<object> { row });
 
-        const string range = "Menu!A1:B";
-        
-        // The new values to apply to the spreadsheet.
-        var requestBody = new BatchUpdateValuesRequest()
-        {
-            ValueInputOption = "USER_ENTERED",
-            Data = new List<ValueRange>
-            {
-                new ValueRange() { Range = range, Values = objParams }
-            }
-        };
-
-        var request = _sheetsService.Spreadsheets.Values.BatchUpdate(requestBody, _spreadsheetId);
-
-        var response = await request.ExecuteAsync(); // For async 
-
-        return JsonUtility.ToJson(response);
-    }
-
-    async void Awake()
-    {
-        ConnectToGoogle();
-
-        SpreadsheetsResource.ValuesResource.GetRequest getRequest = _sheetsService.Spreadsheets.Values.Get(_spreadsheetId, "Menu!A1:B");
-
-        var getResponse = await getRequest.ExecuteAsync();
-        var values = getResponse.Values;
-        if (values != null && values.Count > 0)
-        {
-            foreach (var row in values)
-            {
-                Debug.Log(row[0]);
-                Debug.Log(row[1]);
-            }
-        }
-
-        await UpdateData(new List<List<string>> { new List<string> { "test1", "test2" }, new List<string> { "test3" } });
-        return;
-        string Filepath = Path.Combine(Application.persistentDataPath, "menu.txt");
-
-        MenuItem[] items = null;
-        if (!File.Exists(Filepath))
-        {
-            items = new MenuItem[]
-            {
-                new MenuItem("ESP", 100),
-                new MenuItem("AM", 100, 150),
-                new MenuItem("VAM", 130, 180),
-                new MenuItem("FW", 150, 200),
-                new MenuItem("Lat", 150, 200),
-                new MenuItem("VLat", 180, 230),
-                new MenuItem("Tea", 100),
-            };
-        }
-
-        foreach (var item in items)
-        {
-            var go = Instantiate(MenuItemPrefab, Scroll);
-            foreach (var side in go.GetComponentsInChildren<MenuEntry>())
-            {
-                side.MenuName.text = item.Name;
-                side.Price = side.IsHot ? item.HotPrice : item.ColdPrice;
-            }
-        }
-    }
-
-    [Serializable] private sealed class MenuItem //For JSON serialization
-    {
-        public string Name;
-        public int HotPrice, ColdPrice; // $1.00 = 100.
-        public MenuItem (string Name, int HotPrice, int ColdPrice)
-        {
-            this.Name = Name;
-            this.HotPrice = HotPrice;
-            this.ColdPrice = ColdPrice;
-        }
-        public MenuItem(string Name, int Price) : this(Name, Price, Price) { }
+        await RefreshMenu();
+        //await UpdateData(new List<List<string>> { new List<string> { "test1" } });
     }
 }
